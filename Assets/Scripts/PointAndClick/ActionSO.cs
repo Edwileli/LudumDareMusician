5904a9580ca73ede03be3db2b048ed1aef8e00f0
Edwige Lelièvre <edwige.lelievre@lightbulbcrew.com>
2022-04-02 16:43:50 +0200

    feat: script for actions as Scriptable objects

---
+++ Assets/Scripts/PointAndClick/ActionSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "action_so", menuName = "ScriptableObjects/Action")]
+public class ActionSO : ScriptableObject
+{
+    public string ActionName = "";
+}
