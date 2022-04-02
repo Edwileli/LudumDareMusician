ed61b085e00200f7ef865a0c400999754ea9aff7
Edwige Lelièvre <edwige.lelievre@lightbulbcrew.com>
2022-04-02 13:16:34 +0200

    feat: adding main menu with prefabs for UI

---
+++ Assets/Scripts/UI/MenuButtons.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void PlayGameButton()
+    {
+        Debug.Log("Play");
+    }
+
+    public void OpenHelpWindowButton()
+    {
+        Debug.Log("Open Help");
+    }
+
+    public void QuitGameButton()
+    {
+        Debug.Log("Quit");
+    }
+
+    public void OpenAboutWindowButton()
+    {
+        Debug.Log("Open About");
+    }
+
+}
