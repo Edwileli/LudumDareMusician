b9eff2d11fcb3fbd13cc018f71403657891ad0ec
Edwige Lelièvre <edwige.lelievre@lightbulbcrew.com>
2022-04-03 15:42:14 +0200

    feat: timer working and end scene (empty)

--- Assets/Scripts/Timer/TimerManager.cs
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TimerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeGivenAtStart = 180f;//in seconds
+    public float timeModifiers = 0f;
+    public float timeThresholdForChangeColor = 60f;
+    public Color normalColor = Color.black;
+    public Color colorWhenThereTimeEnds = Color.red;
+
+    private float timeRemaining = 0f;
+    private float timeWhenTheGameBegins = 0f;
+    private Text timerText = null;
+    private TimerText timerTextScript = null;
+
+    void Awake()
     {
-        
+        timerTextScript = FindObjectOfType<TimerText>();
+        if (!timerTextScript)
+        {
+            Debug.Log("timerTextScript missing on " + gameObject.name);
+        }
+        timerText = timerTextScript.GetComponent<Text>();
+        timeWhenTheGameBegins = Time.time;
+        Debug.Log("timeWhenTheGameBegins " + timeWhenTheGameBegins);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddTime(float timeToAdd)
     {
-        
+        timeModifiers += timeToAdd;
+    }
+
+    private void Update()
+    {
+        Debug.Log("Time.time " + Time.time);
+        timeRemaining = timeWhenTheGameBegins + timeGivenAtStart + timeModifiers - Time.time;
+
+        if (timeRemaining < 0)
+        {
+            Debug.Log("End");
+            SceneManager.LoadScene("end_scene", LoadSceneMode.Single);
+        }
+
+        if (timeRemaining < timeThresholdForChangeColor)
+        {
+            timerText.color = colorWhenThereTimeEnds;
+        }
+        else
+        {
+            timerText.color = normalColor;
+        }
+
+        int minutes = Mathf.FloorToInt(timeRemaining / 60F);
+        int seconds = Mathf.FloorToInt(timeRemaining - minutes * 60);
+        string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = niceTime;
     }
 }
