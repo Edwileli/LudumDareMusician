c4053e48321d327a41250c33c1cb6998ea519412
Edwige Lelièvre <edwige.lelievre@lightbulbcrew.com>
2022-04-04 14:38:33 +0200

    fix: cleaning and fixing bug on timer beginning on the intro screen

--- Assets/Scripts/Timer/TimerManager.cs
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -12,10 +10,12 @@ public class TimerManager : MonoBehaviour
     public Color normalColor = Color.black;
     public Color colorWhenThereTimeEnds = Color.red;
 
+    private bool timerHasBegan = false;
     private float timeRemaining = 0f;
     private float timeWhenTheGameBegins = 0f;
     private Text timerText = null;
     private TimerText timerTextScript = null;
+    private TimerPanel timerPanel = null;
 
     void Awake()
     {
@@ -24,33 +24,41 @@ public class TimerManager : MonoBehaviour
         {
             Debug.Log("timerTextScript missing on " + gameObject.name);
         }
+        timerPanel = FindObjectOfType<TimerPanel>();
+        if (!timerPanel)
+        {
+            Debug.Log("timerPanel missing on " + gameObject.name);
+        }
         timerText = timerTextScript.GetComponent<Text>();
-        timerText.gameObject.SetActive(false);//hiding timer before the game starts
+        timerPanel.gameObject.SetActive(false);//hiding timer before the game starts
     }
 
     private void Update()
     {
-        timeRemaining = timeWhenTheGameBegins + timeGivenAtStart + timeModifiers - Time.time;
-
-        if (timeRemaining < 0)
+        if(timerHasBegan)
         {
-            Debug.Log("End");
-            SceneManager.LoadScene("end_scene", LoadSceneMode.Single);
-        }
+            timeRemaining = timeWhenTheGameBegins + timeGivenAtStart + timeModifiers - Time.time;
 
-        if (timeRemaining < timeThresholdForChangeColor)
-        {
-            timerText.color = colorWhenThereTimeEnds;
-        }
-        else
-        {
-            timerText.color = normalColor;
-        }
+            if (timeRemaining < 0)
+            {
+                Debug.Log("End");
+                SceneManager.LoadScene("end_scene", LoadSceneMode.Single);
+            }
 
-        int minutes = Mathf.FloorToInt(timeRemaining / 60F);
-        int seconds = Mathf.FloorToInt(timeRemaining - minutes * 60);
-        string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
-        timerText.text = niceTime;
+            if (timeRemaining < timeThresholdForChangeColor)
+            {
+                timerText.color = colorWhenThereTimeEnds;
+            }
+            else
+            {
+                timerText.color = normalColor;
+            }
+
+            int minutes = Mathf.FloorToInt(timeRemaining / 60F);
+            int seconds = Mathf.FloorToInt(timeRemaining - minutes * 60);
+            string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+            timerText.text = niceTime;
+        }        
     }
 
     public void AddTime(float timeToAdd)
@@ -64,8 +72,9 @@ public class TimerManager : MonoBehaviour
         //init timer
         timeWhenTheGameBegins = Time.time;
         Debug.Log("timeWhenTheGameBegins " + timeWhenTheGameBegins);
+        timerHasBegan = true;
 
         //display timer
-        timerText.gameObject.SetActive(true);
+        timerPanel.gameObject.SetActive(true);
     }
 }
