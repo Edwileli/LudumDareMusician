3e8e103fd6294e1548134cb4990d5e11ae5033de
Edwige Lelièvre <edwige.lelievre@lightbulbcrew.com>
2022-04-04 18:26:23 +0200

    feat: wip combination selection

---
+++ Assets/Scripts/PointAndClick/AActions/ActionSoundCheck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSoundCheck : AAction
+{
+    public override void PerformAction()
+    {
+        base.PerformAction();
+    }
+}
--- Assets/Scripts/PointAndClick/ActionsManager.cs
+++ Assets/Scripts/PointAndClick/ActionsManager.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ActionsManager : MonoBehaviour
 {
     private List<AAction> aActionList = new List<AAction>();
+
+    private List<InteractiveGameObjectSO> selectedObjectsList = new List<InteractiveGameObjectSO>(); //0 is currently selected
 
     void Awake()
     {
@@ -14,17 +17,46 @@ public class ActionsManager : MonoBehaviour
     public List<AAction> FindAvailableActionForAnObject(InteractiveGameObjectSO objectSO)
     {
         List<AAction> availableActions = new List<AAction>();
-       
+
         foreach (ActionSO actionSO in objectSO.ActionsForThisObject)
         {
             foreach (AAction aAction in aActionList)
             {
                 if (aAction.actionSO == actionSO)
                 {
-                    availableActions.Add(aAction);
+                    if (actionSO.ListCombination.Count == 0)
+                    {
+                        availableActions.Add(aAction);
+                    }
+                    else
+                    {
+                        if(actionSO.ListCombination.Count== selectedObjectsList.Count)
+                        {
+                            bool sameLists = Enumerable.SequenceEqual(actionSO.ListCombination.OrderBy(e => e), selectedObjectsList.OrderBy(e => e));
+                            if (sameLists)
+                            {
+                                availableActions.Add(aAction);
+                            }
+                        }
+                    }
                 }
             }
         }
         return availableActions;
+    }
+
+    public void SetCurrentInteractiveObject(InteractiveGameObjectSO interactiveObject)
+    {
+        if (selectedObjectsList[2] != null)
+        {
+            selectedObjectsList[2] = selectedObjectsList[1];
+        }
+
+        if (selectedObjectsList[1] != null)
+        {
+            selectedObjectsList[1] = selectedObjectsList[0];
+        }
+
+        selectedObjectsList[0] = interactiveObject;
     }
 }
